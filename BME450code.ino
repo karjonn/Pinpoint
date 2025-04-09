4256651a6e109bc4728fee4ce936336f290d8416
Karthik Jonnalagadda <karjonn@0587454790.wireless.umich.net>
2025-04-09 16:20:11 -0400

    4/9 edits to account for rotation of polarizer

--- BME450code.ino
+++ BME450code.ino
@@ -5,8 +5,8 @@ int baseline = 0;
 bool calibrated = false;
 
 void setup() {
-  Serial.begin(115200);
-  delay(2000); // Wait for serial monitor
+  Serial.begin(9600);
+  delay(10000); // Wait for serial monitor
   Serial.println("Place normal tissue above the sensor to calibrate...");
   
   // Calibration step
@@ -18,19 +18,29 @@ void setup() {
   baseline = sum / 100;
   calibrated = true;
 
+  Serial.print("################################");
   Serial.print("Calibration complete. Baseline: ");
-  Serial.println(baseline);
+  Serial.println("Baseline: "+ baseline);
 }
 
 void loop() {
   if (!calibrated) return;
 
+  Serial.print("How many readings will you be taking ");
+  int numReadings = Serial.parseInt();
+
   int reading = 0;
-  for (int i = 0; i < 100; i++) {
-    reading += analogRead(PHOTO_PIN);
-    delay(10);
+  Serial.println("----Starting Reading----");
+  for (int i = 0; i < numReadings; i++){
+    for (int j = 0; j < 100; j++) {
+      reading += analogRead(PHOTO_PIN);
+      delay(10);
+    }
+    Serial.println("----ROTATE POLARIZER----");
+    delay(5000);
   }
-  reading = reading / 100;
+
+  reading = reading / (100 * numReadings);
   int delta = reading - baseline;
 
   Serial.print("Reading: ");
@@ -46,4 +56,14 @@ void loop() {
   }
 
   delay(2000);
+  Serial.print("Do you want to continue(y/n)");
+  String ans = Serial.readStringUntil('\n');
 }
+
+
+
+
+
+
+
+
